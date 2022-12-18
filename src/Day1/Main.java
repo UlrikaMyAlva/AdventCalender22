24956645ad8c78c0a3226ecae037e15a89fa528a
Ulrika <ulrika.m.a.eriksson@gmail.com>
2022-12-18 02:22:31 +0100

    Initial commit

---
+++ src/Day1/Main.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        int elfIndex = 0;
+
+        File file = new File("C:\\Users\\Ulrik\\IdeaProjects\\AdventCalender\\src\\Elf\\ElfFood.txt");
+        Scanner sc = new Scanner(file);
+
+        while (sc.hasNextLine())
+            System.out.println(sc.nextLine());
+        if (sc.)
+
+
+
+
+        System.out.println("Hello world!");
+    }
+}
